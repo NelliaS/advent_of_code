27687f382ad12d7fa578f53b99626fe2ac16b82d
NelliaS <nelaslezakova@seznam.cz>
2021-12-04 21:37:58 +0100

    2021/day 4., both parts

---
+++ advent_of_code_2021/day_4/day_4.py
@@ -0,0 +1,170 @@
+from typing import Tuple, List
+
+
+def parse_data(file_name) -> Tuple[List[int], List[int]]:
+    """
+    Read data from a file and save it to 2 variables:
+    - list_of_draws is a list of integers (numbers to be drawn)
+    - list_of_boards is a list of boards, each nested list is one row of a board
+
+    Args:
+        file_name (str): name of file in folder / or absolute path
+
+    Returns:
+        Tuple[List[int], List[int]]: (list_of_draws, boards_data)
+    """
+    with open(file_name, encoding="utf-8") as f:
+        lines = f.readlines()
+
+        # make a list of draws
+        list_of_draws = list(map(int, lines[0].rstrip().split(",")))
+
+        # make a nested list of boards' numbers
+        list_of_boards = []
+        one_board_data = []
+        for line in lines[2:]:
+            if line == "\n":
+                list_of_boards.append(one_board_data)
+                one_board_data = []
+            else:
+                one_row = line.rstrip().split()
+                one_row = list(map(int, one_row))
+                one_board_data.append(one_row)
+        # append last board
+        list_of_boards.append(one_board_data)
+
+    return list_of_draws, list_of_boards
+
+
+class Board:
+    """
+    Every board starts as a list, where an each nested list represents one row.
+    A "board" variable can eg. look like:
+        [
+            [22, 13, 17, 11, 0],
+            [8, 2, 23, 4, 24],
+            [21, 9, 14, 16, 7],
+            [6, 10, 3, 18, 5],
+            [1, 12, 20, 15, 19]
+        ]
+    """
+
+    def __init__(self, one_board_data):
+        self.marked = []
+        self.all_numbers = []
+        self.board = one_board_data
+
+        for row in self.board:
+            for number in row:
+                self.all_numbers.append(number)
+
+    def mark_number(self, number, position) -> None:
+        """
+        Mark drawn number in list "board"
+        Marked number will be transformed into tuple, with 'd' as second element
+        Eg. to mark 9 in row [21, 9, 14] will modify board to [21, (9, 'd'), 14]
+        Also add drawn number into a list "marked"
+
+        Args:
+            number (int): number which has been drawn
+            position (tuple): number of row and position in list "board"
+        """
+        row, column = position
+        self.board[row][column] = (number, "d")
+        self.marked.append(number)
+
+    def find_number_to_mark(self, drawn_number) -> None:
+        """
+        Search board for a drawn number.
+        If there is any, mark it with a method mark_number()
+
+        Args:
+            drawn_number (int): number which have been drawn
+        """
+        for index_row, row in enumerate(self.board):
+            for index_column, number in enumerate(row):
+                if drawn_number == number:
+                    self.mark_number(drawn_number, (index_row, index_column))
+                    return
+
+    def has_won(self):
+        """
+        Determine if board have won "bingo" in one of the rows or columns.
+        That means all numbers in specific row or column have been marked.
+
+        Returns:
+            bool: True/False
+        """
+        # win in row
+        count_marks = 0
+        marks_to_win = len(self.board)
+
+        for row in self.board:
+            for el in row:
+                if type(el) != int:
+                    count_marks += 1
+            if count_marks == marks_to_win:
+                return True
+            else:
+                count_marks = 0
+
+        # win in column
+        count_marks = 0
+        marks_to_win = len(self.board[0])
+        for position in range(len(self.board[0])):
+            for row_number in range(len(self.board)):
+                if type(self.board[row_number][position]) != int:
+                    count_marks += 1
+            if count_marks == marks_to_win:
+                return True
+            else:
+                count_marks = 0
+
+        return False
+
+    def calculate_score(self, last_number_drawn):
+        """
+        Calculate score of board, sum all unmarked numbers and multiply by
+        last number drawn.
+
+        Args:
+            last_number_drawn (int): last number drawn
+
+        Returns:
+            int: score of board
+        """
+        unmarked = list(set(self.all_numbers) - set(self.marked))
+        sum_unmarked = sum(unmarked)
+        return sum_unmarked * last_number_drawn
+
+
+def apply_one_round(winning, game_boards, drawn_number):
+    remaining_boards = game_boards[::]
+
+    for board in game_boards:
+        board.find_number_to_mark(drawn_number)
+        if board.has_won():
+            winning.append(board.calculate_score(drawn_number))
+            remaining_boards.remove(board)
+
+    return winning, remaining_boards
+
+
+def main(file_name):
+    list_of_draws, list_of_boards = parse_data(file_name)
+    game_boards = []
+    winning = []
+
+    for board_data in list_of_boards:
+        game_boards.append(Board(board_data))
+
+    for drawn_number in list_of_draws:
+        winning, game_boards = apply_one_round(winning, game_boards, drawn_number)
+
+    return winning[0], winning[-1]
+
+
+# part 1 + 2
+part_1, part_2 = main("day_4.txt")
+print(f"Part 1: {part_1}")
+print(f"Part 2: {part_2}")
---
+++ advent_of_code_2021/day_4/test_day_4.py
@@ -0,0 +1,74 @@
+from pytest import mark, raises
+from day_4 import parse_data, Board, main
+
+
+def test_parse_data() -> None:
+    list_of_draws = [7, 4, 9, 5, 11, 17, 23, 2, 0, 14, 21, 24, 10, 16, 
+                     13, 6, 15, 25, 12, 22, 18, 20, 8, 19, 3, 26, 1
+                    ]
+    boards_data = [
+        [
+            [22, 13, 17, 11, 0],
+            [8, 2, 23, 4, 24],
+            [21, 9, 14, 16, 7],
+            [6, 10, 3, 18, 5],
+            [1, 12, 20, 15, 19],
+        ],
+        [
+            [3, 15, 0, 2, 22],
+            [9, 18, 13, 17, 5],
+            [19, 8, 7, 25, 23],
+            [20, 11, 10, 24, 4],
+            [14, 21, 16, 12, 6],
+        ],
+        [
+            [14, 21, 17, 24, 4],
+            [10, 16, 15, 9, 19],
+            [18, 8, 23, 26, 20],
+            [22, 11, 13, 6, 5],
+            [2, 0, 12, 3, 7],
+        ],
+    ]
+    assert parse_data("day_4_test.txt") == (list_of_draws, boards_data)
+
+
+def test_Board_mark_number() -> None:
+    board = Board([[22, 13, 17], [8, 2, 23], [21, 9, 14]])
+    board.mark_number(2, (1, 1))
+    assert board.board == [[22, 13, 17], [8, (2, "d"), 23], [21, 9, 14]]
+    assert board.marked == [2]
+
+
+def test_Board_has_won_row() -> None:
+    board = Board(
+        [[22, (13, "d"), 17], [8, (2, "d"), 23], [(21, "d"), (9, "d"), (14, "d")]]
+    )
+    assert board.has_won() == True
+    board = Board([[22, (13, "d"), 17], [8, (2, "d"), 23], [(21, "d"), 9, 14]])
+    assert board.has_won() == False
+
+
+def test_Board_has_won_column() -> None:
+    board = Board([[22, 13, (17, "d")], [8, 2, (23, "d")], [21, 9, (14, "d")]])
+    assert board.has_won() == True
+    board = Board([[(22, "d"), 13, 17], [8, 2, (23, "d")], [21, 9, (14, "d")]])
+    assert board.has_won() == False
+
+
+def test_Board_calculate_score() -> None:
+    board = Board([[22, 13, (17, "d")], [8, 2, (23, "d")], [21, 9, (14, "d")]])
+    board.all_numbers = [22, 13, 17, 8, 2, 23, 21, 9, 14]
+    board.marked = [17, 23, 14]
+    assert board.calculate_score(14) == 1050
+
+
+def test_Board_find_number_to_mark() -> None:
+    board = Board([[22, 13, (17, "d")], [8, 2, 23]])
+    board.find_number_to_mark(2)
+    assert board.board == [[22, 13, (17, "d")], [8, (2, "d"), 23]]
+
+
+def test_main() -> None:
+    part_1, part_2 = main("day_4_test.txt")
+    assert part_1 == 4512
+    assert part_2 == 1924
