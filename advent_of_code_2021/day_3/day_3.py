9afc67f560b0810c148f11d05161954c646909dc
NelliaS <nelaslezakova@seznam.cz>
2021-12-04 13:00:45 +0100

    add doctrings in day_3

--- advent_of_code_2021/day_3/day_3.py
+++ advent_of_code_2021/day_3/day_3.py
@@ -8,6 +8,18 @@ with open("day_3.txt", encoding="utf-8") as f:
 
 
 def count_vertically_most_common_bits(numbers) -> Dict[int, Dict[str, int]]:
+    """
+    For each position in list of binary numbers count number 0 and number 1.
+    Result save into nested dictionary.
+    Eg. [11, 01] -> {0: {'0': 1, '1': 1}, 1: {'0': 0, '1': 2}}
+
+    Args:
+        numbers (list): list of numbers in binary format
+
+    Returns:
+        Dict[int, Dict[str, int]]: keys are numbered positions,
+        values are dictionaries, with 2 keys ('0' and '1') and counts as values.
+    """
     bits_in_lines = {}
     for digit in [*range(len(numbers[0]))]:
         bits_in_lines[digit] = {"0": 0, "1": 0}
@@ -20,6 +32,17 @@ def count_vertically_most_common_bits(numbers) -> Dict[int, Dict[str, int]]:
 
 
 def count_gamma_and_epsilon_rate(bits_in_lines) -> Tuple:
+    """
+    Gamma rate is calculated from prevailing (digit (0 or 1) in each position
+    Epsilon rate is opposite number (eg. '010' was made from gamma rate '101')
+
+    Args:
+        bits_in_lines (dict): nested dictionary, counted '0' and '1' for each
+                              position in list of binary numbers
+
+    Returns:
+        Tuple: gamma and epsilon rate
+    """
     gamma_rate = ""
     epsilon_rate = ""
     for value in bits_in_lines.values():
@@ -31,23 +54,33 @@ def count_gamma_and_epsilon_rate(bits_in_lines) -> Tuple:
     return gamma_rate, epsilon_rate
 
 
-def count_O2_and_CO2_ratings(numbers, bits_in_lines) -> Tuple:
-    O2_rating = filter_list(numbers[::], bits_in_lines, "most")
-    CO2_rating = filter_list(numbers[::], bits_in_lines, "least")
-    return O2_rating, CO2_rating
-
-
 def filter_list(numbers, bits_in_lines, criteria) -> str:
+    """
+    Determine 'most' or 'least' common value ('0' or '1') in the current position,
+    and keep in a list only those bit numbers with that bit in that position.
+
+    Args:
+        numbers (list): list of numbers in binary format
+        bits_in_lines (dict): nested dictionary, counted '0' and '1' for each
+                              position in list of binary numbers
+        criteria (str): criteria for comparison (values 'most' or 'least')
+
+    Returns:
+        str: resulting binary number after filtering
+    """
     kept_numbers = numbers[::]
     i = 0
     while len(kept_numbers) > 1:
         new_kept_numbers = []
+
         number_O_count = bits_in_lines[i]["0"]
         number_1_count = bits_in_lines[i]["1"]
         number_to_keep = compare_numbers(number_O_count, number_1_count, criteria)
+
         for number in kept_numbers:
             if number[i] == number_to_keep:
                 new_kept_numbers.append(number)
+
         kept_numbers = new_kept_numbers
         bits_in_lines = count_vertically_most_common_bits(kept_numbers)
         i += 1
@@ -56,6 +89,20 @@ def filter_list(numbers, bits_in_lines, criteria) -> str:
 
 
 def compare_numbers(number_0, number_1, criteria) -> str:
+    """
+    Compare numbers according to a criteria ('most' or 'least').
+    Criteria 'most' returns the number of which count is bigger.
+    Criteria 'least' returns the number of which count is lesser.
+    When counts are equal, criteria 'most' returns '1', and 'least' returns '0'
+
+    Args:
+        number_0 (int): count of numbers '0'
+        number_1 (int): count of numbers '1'
+        criteria (str): criteria for comparison
+
+    Returns:
+        str: resulting number (values '0' or '1')
+    """
     if criteria == "most":
         if number_0 == number_1:
             return "1"
@@ -73,7 +120,9 @@ def main() -> None:
     gamma_rate, epsilon_rate = count_gamma_and_epsilon_rate(bits_in_lines)
     result1 = int(gamma_rate, 2) * int(epsilon_rate, 2)
     print(f"Part 1 result: {result1}")
-    O2_rating, CO2_rating = count_O2_and_CO2_ratings(list_of_numbers, bits_in_lines)
+
+    O2_rating = filter_list(numbers[::], bits_in_lines, "most")
+    CO2_rating = filter_list(numbers[::], bits_in_lines, "least")
     result2 = int(O2_rating, 2) * int(CO2_rating, 2)
     print(f"Part 2 result: {result2}")
 
