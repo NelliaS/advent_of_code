6f6bfe612815d4870d8ac11674b80a66051ccff5
Nela Slezáková <nela.slezakova@flowerchecker.com>
2022-11-18 22:00:36 +0100

    2015 - day 8 completed, both parts

--- advent_of_code_2015/day_8.py
+++ advent_of_code_2015/day_8.py
@@ -30,15 +30,32 @@ def count_string_chars():
             line = line.replace('\\\\', '1')
         while '\\"' in line:
             i = line.index('\\"')
-            line = line[:i] + line[i+1:]        # only double quote remains
+            line = line[:i] + line[i + 1 :]  # only double quote remains
         while '\\x' in line:
             i = line.index('\\x')
-            if line[i+2] in hexadecimal_chars and line[i+3] in hexadecimal_chars:
-                line = line[:i] + '1' + line[i+4:]      # replace it with single character "1"
+            if line[i + 2] in hexadecimal_chars and line[i + 3] in hexadecimal_chars:
+                line = line[:i] + '1' + line[i + 4 :]  # replace it with single character "1"
         string_chars_list.append(line)
     for el in string_chars_list:
         string_chars += len(el)
     return string_chars
 
 
+def count_encoded_string():
+    count = 0
+    with open('day_8.txt') as f:
+        for line in f:
+            line = line.rstrip('\n')[1:-1]
+            count += 6  # for surrounding strings
+            for char in line:
+                if char == '"':
+                    count += 2
+                elif char == '\\':
+                    count += 2
+                else:
+                    count += 1
+    return count
+
+
 print(f'Result of part 1: "{count_code_chars() - count_string_chars()}"')
+print(f'Result of part 2: "{count_encoded_string() - count_code_chars()}"')
