c04c73074a007bf6655c24f8ac7bc04a5e7e7100
Nela Slezáková <nela.slezakova@flowerchecker.com>
2022-11-20 13:35:11 +0100

    2015 - finished last day

--- advent_of_code_2015/day_10.py
+++ advent_of_code_2015/day_10.py
@@ -4,7 +4,7 @@ with open('day_10.txt') as f:
 
 def parse_sequence(sequence):
     """
-    Take a sequence of numbers and parse them into a list, group the same 
+    Take a sequence of numbers and parse them into a list, group the same
     numbers into one element of the list
 
     Args:
@@ -35,34 +35,34 @@ def make_new_sequence(parsed_sequence):
     Apply look-and-say rule to a whole parsed_sequence
 
     Args:
-        parsed_sequence (list): each element consists of one type of number 
+        parsed_sequence (list): each element consists of one type of number
 
     Returns:
         list: parsed sequence after applying look-and-say rule
     """
-    
+
     new_sequence = []
-    
+
     for element in parsed_sequence:
         new_element = parse_sequence(apply_look_and_say(element))
         new_sequence.extend(new_element)
-    
-    # correct the parsing (same numbers are in one element) 
+
+    # correct the parsing (same numbers are in one element)
     return parse_sequence(new_sequence)
 
 
 def apply_look_and_say(element):
     """
     Apply look-and-say rule to an element
-    New element consist of number which represents length of string 
-    and then one of the digits of the element 
+    New element consist of number which represents length of string
+    and then one of the digits of the element
 
     Args:
-        element (str): various number of digits of one type 
-        
-    Returns: 
-        list: new string after applying look and say rule 
-    
+        element (str): various number of digits of one type
+
+    Returns:
+        list: new string after applying look and say rule
+
     """
     lenght = len(element)
     number = element[0]
@@ -79,13 +79,13 @@ def repeat_game(number, sequence):
         sequence (str): initial sequence of numbers
 
     Returns:
-        str: resulting string after applying the look-and-say rule number-times 
+        str: resulting string after applying the look-and-say rule number-times
     """
     parsed_sequence = parse_sequence(sequence)
 
     for repetition in range(number):
         parsed_sequence = make_new_sequence(parsed_sequence)
-    
+
     return ''.join(parsed_sequence)
 
 
@@ -105,10 +105,10 @@ def test_apply_look_and_say():
 def test_make_new_sequence():
     assert make_new_sequence(['1']) == ['11']
     assert make_new_sequence(['11']) == ['2', '1']
-    assert make_new_sequence(['2', '1']) == ['1', '2', '11']   
+    assert make_new_sequence(['2', '1']) == ['1', '2', '11']
     assert make_new_sequence(['1', '2', '11']) == ['111', '22', '1']
     assert make_new_sequence(['111', '22', '1']) == ['3', '1', '22', '11']
-  
+
 
 def test_repeat_game():
     assert repeat_game(5, '1') == '312211'
@@ -116,7 +116,7 @@ def test_repeat_game():
 
 # Results:
 
-# part 1     
+# part 1
 print(len(repeat_game(40, initial_sequence)))
 
 # part 2
