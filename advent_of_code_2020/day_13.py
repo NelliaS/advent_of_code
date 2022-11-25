f7e6e4bfce462999cfa314b4114eb2c90334bb6b
Nela Slezáková <nela.slezakova@flowerchecker.com>
2022-11-25 18:00:55 +0100

    2020 - day 13 completed, part2 solved

--- advent_of_code_2020/day_13.py
+++ advent_of_code_2020/day_13.py
@@ -1,3 +1,4 @@
+import math
 
 with open('day_13.txt') as f:
     array = f.read().split('\n')
@@ -5,33 +6,67 @@ with open('day_13.txt') as f:
     schedule = array[1].split(',')
 
 
-# PART 1
-
-def choose_bus(arrival_time, buses):
-    '''
+def find_closest_bus(arrival_time, schedule):
+    """
     Look for searched bus through addition of time.
     Return earliest bus, for which division of time and its number will be zero.
-    '''
+    """
+    buses = [int(x) for x in schedule if x != 'x']
     departure_time = arrival_time
     while True:
-        for ID in buses:
-            if departure_time % ID == 0:
-                bus_chosen = ID
-                return bus_chosen, departure_time
+        for bus_id in buses:
+            if departure_time % bus_id == 0:
+                return (departure_time - arrival_time) * bus_id
         departure_time += 1
 
 
-def find_closest_bus(arrival_time, buses):
-    '''
-    Buses are numbered. They take loop, leaving at times multiplied by their number.
-    Find closest bus, after arrival_time.
-    Return number of earliest bus you can take to the airport multiplied by number of waited minutes.
-    '''
-    buses = []
-    [buses.append(int(x)) for x in schedule if x != 'x']
-    bus_chosen, departure_time = choose_bus(arrival_time, buses)
-    waiting = departure_time - arrival_time
-    return waiting * bus_chosen
+# Previous - extremely slow solution, even though optimized as well as I could
+
+# def find_timestamp_of_schedule(schedule):
+#     template = [(int(bus), schedule.index(bus)) for bus in schedule if bus != 'x']
+#     chosen_bus, time_to_sync = max(template)
+#     departures = {bus: departure - time_to_sync for bus, departure in template if bus != chosen_bus}
+#     divisor_rules = {
+#         19: ['+', 2],
+#         17: ['-', 5],
+#         23: ['+', 7],
+#         13: ['+', 4],
+#         29: ['+', 3],
+#         37: ['-', 11],
+#         41: ['-', 4],
+#         379: ['-', 341],
+#     }
+#
+#     for timestamp in count(99999999999708, step=chosen_bus):
+#         for bus_id, diff in departures.items():
+#             timestamp_to_check = str(timestamp + diff)
+#             operation, multiply_by = divisor_rules[bus_id]
+#             n1, n2 = int(timestamp_to_check[:-1]), int(timestamp_to_check[-1]) * multiply_by
+#             result = n1 + n2 if operation == '+' else n1 - n2
+#             if not result % bus_id == 0:
+#                 break
+#             else:
+#                 if bus_id == 19:  # last on the list
+#                     return timestamp + min(departures.values())
 
 
-print(find_closest_bus(arrival_time, schedule))     # part 1
+def find_timestamp_of_schedule(schedule):
+    """
+    We are looking for number, which can be divided by product of bus ids to give result timestamp as a remainder.
+    Such number is sum of multiplications of partial product * remainder * modular multiplicative inverse
+
+    Heavily based on Elvin Lee solution, check it out:
+        https://github.com/elvinyhlee/advent-of-code-2020-python/blob/master/day13.py
+    """
+    template = [(int(bus_id), int(bus_id) - schedule.index(bus_id)) for bus_id in schedule if bus_id != 'x']
+    product = math.prod([bus_id for bus_id, _ in template])
+    number = 0
+    for bus_id, remainder in template:
+        partial_product = product // bus_id
+        number += partial_product * remainder * pow(partial_product, -1, bus_id)
+
+    return number % product
+
+
+print(f'Result of part 1: "{find_closest_bus(arrival_time, schedule)}"')
+print(f'Result of part 2: "{find_timestamp_of_schedule(schedule)}"')
