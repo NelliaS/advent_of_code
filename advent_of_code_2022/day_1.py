6591b91623f83990ac1a8addd25057a992147af3
Nela Slezáková <nela.slezakova@flowerchecker.com>
2022-12-01 07:42:47 +0100

    2022 - day 1 completed, both parts

---
+++ advent_of_code_2022/day_1.py
@@ -0,0 +1,18 @@
+import heapq
+
+with open('day_1.txt') as f:
+    elves_calories = [sum((map(int, items.split()))) for items in f.read().split('\n\n')]
+
+
+def part1(elves_calories: list[int]) -> int:
+    """Return the most calories taken by 1 elf."""
+    return max(elves_calories)
+
+
+def part2(elves_calories: list[int]) -> int:
+    """Return sum of the most calories taken by 3 elves."""
+    return sum(heapq.nlargest(3, elves_calories))
+
+
+print(f'Result of part 1: "{part1(elves_calories)}"')
+print(f'Result of part 1: "{part2(elves_calories)}"')
