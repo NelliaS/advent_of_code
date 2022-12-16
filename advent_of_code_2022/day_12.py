f68ec80c67cfd72a596583418e14c50161063586
Nela Slezáková <nela.slezakova@flowerchecker.com>
2022-12-16 18:19:25 +0100

    2022 - day 12 completed, both parts

--- advent_of_code_2022/day_12.py
+++ advent_of_code_2022/day_12.py
@@ -1,20 +1,15 @@
 import math
 from collections import defaultdict
 
-from line_profiler_pycharm import profile
-from icecream import ic
-
 import networkx as nx
+from icecream import ic
 
 
 class GoalFinder:
-    # @profile
     def __init__(self):
         maze = {}
-        maze_raw = []
-        with open('day_12_example.txt') as f:
+        with open('day_12.txt') as f:
             for y, line in enumerate(f):
-                maze_raw.append([ord(letter) - 96 for letter in line.rstrip()])
                 for x, number in enumerate([ord(letter) - 96 for letter in line.rstrip()]):
                     if number == -13:
                         self.start_point = (x, y)
@@ -24,50 +19,37 @@ class GoalFinder:
                         number = 26
                     maze[(x, y)] = number
         self.maze = maze
-        self.maze_measures = (len(maze_raw[0]) - 1, len(maze_raw) - 1)
-        self.steps_to_field = {k: math.inf for k in self.maze}
-        self.neighbours = defaultdict(list)
+        self.maze_measures = (len(line.rstrip()) - 1, y)
+
+        neighbours = defaultdict(list)
         for x0, y0 in self.maze:
             for x1, y1 in [(x0 + 1, y0), (x0 - 1, y0), (x0, y0 - 1), (x0, y0 + 1)]:
                 if 0 <= x1 <= self.maze_measures[0] and 0 <= y1 <= self.maze_measures[1]:
                     if self.maze[(x1, y1)] - self.maze[(x0, y0)] <= 1:
-                        self.neighbours[(x0, y0)].append((x1, y1))
-        ic(self.neighbours)
-        # ic(self.steps_to_field)
-        # ic(maze_raw)
-        # ic(maze)
-        # ic(self.maze_measures)
-        self.shortest_route = math.inf
+                        neighbours[(x0, y0)].append((x1, y1))
 
-    # @profile
+        self.graph = nx.DiGraph()
+        self.graph.add_nodes_from(neighbours.keys())
+        for node, neighbours in neighbours.items():
+            for neighbour_node in neighbours:
+                self.graph.add_edge(node, neighbour_node)
+
     def part1(self):
-        self.one_route(self.start_point, 0, ())
+        return nx.shortest_path_length(self.graph, source=self.start_point, target=self.goal_point)
 
-    # @profile
-    def one_route(self, coord: tuple, count_steps: int, steps: tuple):
-        steps = steps + (coord,)
-        self.steps_to_field[coord] = len(steps)
-
-        possible_steps = [
-            neighbour for neighbour in self.neighbours[coord] if count_steps < self.steps_to_field[neighbour]
-        ]
-
-        if self.goal_point in possible_steps:
-            # ic(f'goal visible after {steps} and {count_steps}')
-            if (count_steps + 1) < self.shortest_route:
-                self.shortest_route = count_steps + 1
-                return
-        elif count_steps > self.shortest_route or not possible_steps:
-            return
-        else:
-            count_steps += 1
-            if count_steps > 500:
-                return
-
-            for new_coord in possible_steps:
-                self.one_route(coord=new_coord, count_steps=count_steps, steps=steps)
+    def part2(self):
+        shortest_route = math.inf
+        possible_starts = [key for key, value in self.maze.items() if value == 1]
+        for start in possible_starts:
+            try:
+                steps = nx.shortest_path_length(self.graph, source=start, target=self.goal_point)
+                if steps < shortest_route:
+                    shortest_route = steps
+            except nx.exception.NetworkXNoPath:
+                ...
+        return shortest_route
 
 
 goal_finder = GoalFinder()
-goal_finder.part1()
-ic(goal_finder.shortest_route)
+print(f'Result of part 1: "{goal_finder.part1()}"')
+print(f'Result of part 1: "{goal_finder.part2()}"')
